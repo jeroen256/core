871512daf791a8d39e41fc6c742c2d5f2852859a
Jeroen256 Linux <Jeroen256@Linux>
2018-11-17 10:47:46 +0100

    [fix] MySqlClient.MySqlException: Specified key was too long

--- Server/LibraryContext.cs
+++ Server/LibraryContext.cs
@@ -12,7 +12,7 @@ namespace Server
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-      optionsBuilder.UseMySQL("server=localhost;database=library;user=root;password=Hoi123!");
+      optionsBuilder.UseMySQL("server=localhost;database=library;user=root;password=");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -32,6 +32,12 @@ namespace Server
         entity.HasOne(d => d.Publisher)
           .WithMany(p => p.Books);
       });
+
+      // MySQL and EntityFrameworks migrations do not get along very well. Specifically, the MigrationHistory table has a primary key that is too large for MySQL
+      // https://github.com/IdentityServer/IdentityServer2/issues/657
+      // Exception on android was: MySql.Data.MySqlClient.MySqlException (0x80004005): Specified key was too long; max key length is 1000 bytes
+      // Fixed by adding this line: (a string key is too long, we need to restrict the maxlength)
+      modelBuilder.Entity<Book>(entity => entity.Property(m => m.ISBN).HasMaxLength(80));
     }
   }
 }
